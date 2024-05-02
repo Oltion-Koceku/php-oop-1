a5a9af7755c77adcc0168df07b755f75cd670286
Oltion-Koceku <koceku17@gmail.com>
2024-05-02 15:49:09 +0200

    [bonus]

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,6 +1,49 @@
 <?php
 
 class Movie{
+  // dichiaro le variabili
+  private $name ;
+  private $minute ;
+  private $year ;
+  private $genere;
 
+
+  public $autor;
+
+
+  //con il construct do il valore alle variabili
+  function __construct(string $_name, string $_minute, string $_year, string $_autor, array $_genere){
+  
+  $this -> setName($_name);
+  $this -> setMinute($_minute);
+  $this -> setYear($_year);
+  $this -> getGenere($_genere);
+  $this -> autor = $_autor;
+
+  }
+  // Funzioni per poter dare un valore alle variabili private
+
+    public function setName($_name){
+      $this -> name = $_name;
+    }
+    public function setMinute($_minute){
+      $this -> minute = $_minute;
+    }
+    public function setYear($_year){
+      $this -> year = $_year;
+    }
+
+
+    //funzione di compliazione film
+
+    public function getFullFilm(){
+      // scritto tutto attaccato
+      $genereDaStampare = implode(", " , $this->genere);
+      echo  "$this->name, $this->minute, $this->year, Generi: $genereDaStampare";
+    }
+
+    public function getGenere($_genere){
+      $this->genere = $_genere;
+    }
 
 };
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php 
+  // importo la mia classe
+ 
+  require_once __DIR__ . '/Model/Movie.php';
+ 
+  // uso la mia classe
 
-  require_once __DIR__ . '/Model/Movie.php'
+  $first_movie = new Movie('The Avenger', '142' . 'minuti', '2012', 'Josh Whedon', ['azione', 'romanzo', 'giallo']);
 
+  var_dump($first_movie)
 ?>
 
 
@@ -14,6 +20,11 @@
   <title>Movie</title>
 </head>
 <body>
-  
+  <h1>Films</h1>
+  <ul>
+    <li>
+      <?php $first_movie->getFullFilm()  ?>
+    </li>
+  </ul>
 </body>
 </html>
